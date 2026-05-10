75a66cbe920171eac544ea000f1adf5cfb95823f
tharushadineshan792-coder <tharushadineshan792@gmail.com>
2026-05-10 10:04:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,10 +14,10 @@ ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK,
 SEEDR_MAIL: '',
 SEEDR_PASSWORD: '',
 SUDO: '',//
-DB_NAME: 'thaaruuu',
+DB_NAME: 'Shan',
 LANG: 'SI',
-OWNER_NUMBER: '94711726564'
+OWNER_NUMBER: '94711726564',
+LOGO: process.env.LOGO === undefined ? 'https://telegra.ph/file/38a4ba6f7a51c15f5feaf.jpg' : process.env.LOGO
 
 };
 //GITHUB_AUTH_TdOKEN: 'ouvnI0xSDsmfWA1filVxx.SZ0vJGYkjlC5VX54U0e10',
-
